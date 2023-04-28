c666bf2baf68e81e24c583f5954436f1b68f67fe
Stephen Nicholson <stephennicholson14@gmail.com>
2023-04-28 10:57:22 +0100

    Updating formatting and pyproject.toml

--- data_exercises/main.py
+++ data_exercises/main.py
@@ -1,9 +1,11 @@
 """ Script to complete data exercises """
 import pandas as pd
 
+
 def main() -> None:
-    data = pd.read_csv("../tbankdata/accounts.csv")
-    data.head()
+    data = pd.read_csv("/home/stephen/projects/data-exercises/tbankdata/accounts.csv")
+    print(data.head())
+
 
 if __name__ == "__main__":
     main()
---
+++ data_exercises/transformations.py
@@ -0,0 +1,6 @@
+""" Contains functions for data exercises """
+import pandas as pd
+
+
+def count_accounts_holders_title(account_data: pd.DataFrame) -> None:
+    pass
