630ea4b9e38dc4d4a13006f5d315a44576f2b782
Stephen Nicholson <stephen.nicholson14@gmail.com>
2023-04-30 09:16:54 +0100

    Second exercise

---
+++ data_exercises/bank_data.py
@@ -0,0 +1,161 @@
+""" Contains class for data exercises """
+import logging
+import numpy as np
+import pandas as pd
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+fh = logging.FileHandler("results.log")
+fh.setLevel(logging.DEBUG)
+logger.addHandler(fh)
+
+
+class BankData:
+    """Class for accounts and transactions data"""
+
+    def __init__(
+        self, account_data: pd.DataFrame, transaction_data: pd.DataFrame
+    ) -> None:
+        self.account_data = self._preprocess_accounts(account_data=account_data)
+        self.transaction_data = self._preprocess_transactions(
+            transaction_data=transaction_data
+        )
+
+    @staticmethod
+    def _preprocess_accounts(account_data: pd.DataFrame) -> pd.DataFrame:
+        account_data.loc[account_data["title"] == "Mr.", "title"] = "Mr"
+        account_data.loc[account_data["title"] == "Mrs.", "title"] = "Mrs"
+
+        account_data.replace(" ", np.nan, inplace=True)
+        account_data.dropna(subset=["account_type"], inplace=True)
+
+        return account_data
+
+    @staticmethod
+    def _preprocess_transactions(transaction_data: pd.DataFrame) -> pd.DataFrame:
+        transaction_data["trans_date"] = pd.to_datetime(transaction_data["trans_date"])
+
+        return transaction_data
+
+    def count_accounts_holders_title(self) -> None:
+        """Counts the number of account holders with each title
+
+        Returns:
+            None
+        """
+        result: pd.DataFrame = self.account_data.groupby(["title"])[
+            "account_number"
+        ].count()
+        logger.info(
+            "Number of account holders with each title - \n %s \n", result.to_string()
+        )
+
+    def count_account_holders_title_account_type(self) -> None:
+        """Produces a cross-table showing the number of account holders broken down by title and
+        account_type (i.e. a count of the number of people with each combination of possible
+        account types and titles)
+
+        Returns:
+            None
+        """
+        result: pd.DataFrame = (
+            self.account_data.groupby(["title", "account_type"])["account_number"]
+            .count()
+            .unstack()
+        )
+        logger.info(
+            "Number of account holders by title and account type - \n %s \n",
+            result.to_string(),
+        )
+
+    def avg_overdraft_limit(self) -> None:
+        """Produce a cross-table showing the average overdraft_limit by title and account_type
+
+        Returns:
+            None
+        """
+        result: pd.DataFrame = (
+            self.account_data.groupby(["title", "account_type"])["overdraft_limit"]
+            .mean()
+            .unstack()
+        )
+        logger.info(
+            "average overdraft_limit by title and account_type - \n %s \n",
+            result.to_string(),
+        )
+
+    def sum_overdraft_limit(self) -> None:
+        """Produces a cross-table showing the aggregate overdraft_limit (sum of) by title
+        and account_type
+
+        Returns:
+            None
+        """
+        result: pd.DataFrame = (
+            self.account_data.groupby(["title", "account_type"])["overdraft_limit"]
+            .sum()
+            .unstack()
+        )
+        logger.info(
+            "sum of overdraft_limit by title and account type - \n %s \n",
+            result.to_string(),
+        )
+
+    def transaction_aggregations(self) -> pd.DataFrame:
+        """counts the number of transactions and computes
+        the total value of those transactions for each customer
+
+        Returns:
+            pd.DataFrame: DataFrame
+        """
+        result: pd.DataFrame = (
+            self.transaction_data.groupby("acc_number")["amount"]
+            .agg(["sum", "count"])
+            .reset_index()
+            .rename(
+                columns={
+                    "acc_number": "account_number",
+                    "sum": "trans_total",
+                    "count": "n_transactions",
+                }
+            )
+        )
+
+        joined = self.account_data.merge(result, on="account_number", how="left")
+
+        logger.info(
+            "transactions aggregations - \n %s \n",
+            result.to_string(max_rows=10),
+        )
+
+        return joined
+
+    def transaction_aggregations_monthly(self) -> pd.DataFrame:
+        """Calculates the number and total value of transactions for
+        each month for which there are transactions.
+
+        Returns:
+            pd.DataFrame: DataFrame
+        """
+        result: pd.DataFrame = (
+            self.transaction_data.groupby(
+                ["acc_number", self.transaction_data.trans_date.dt.strftime("%m-%y")]
+            )["amount"]
+            .agg(["sum", "count"])
+            .reset_index()
+            .rename(
+                columns={
+                    "acc_number": "account_number",
+                    "sum": "trans_total",
+                    "count": "n_transactions",
+                }
+            )
+        )
+
+        logger.info(
+            "transactions aggregations monthly - \n %s \n",
+            result.to_string(max_rows=10),
+        )
+
+        return result
--- data_exercises/main.py
+++ data_exercises/main.py
@@ -1,19 +1,73 @@
 """ Reads bank data and writes to log file """
+from pprint import pprint
 import pandas as pd
+from sqlalchemy import create_engine, text
 
-from data_exercises.accounts import Accounts
+from data_exercises.bank_data import BankData
 
 
 def main() -> None:
     """Reads bank data and writes to log file"""
-    data: pd.DataFrame = pd.read_csv(
-        "/home/stephen/projects/data-exercises/tbankdata/accounts.csv"
+    account_data: pd.DataFrame = pd.read_csv(
+        "/home/stephen/data-exercises/tbankdata/accounts.csv"
     )
 
-    accounts: Accounts = Accounts(account_data=data)
+    transaction_data: pd.DataFrame = pd.read_csv(
+        "/home/stephen/data-exercises/tbankdata/current-acc-trans.csv"
+    )
 
-    result = accounts.sum_overdraft_limit()
-    print(result)
+    bank_data: BankData = BankData(
+        account_data=account_data, transaction_data=transaction_data
+    )
+
+    bank_data.count_accounts_holders_title()
+    bank_data.count_account_holders_title_account_type()
+    bank_data.avg_overdraft_limit()
+    bank_data.sum_overdraft_limit()
+
+    transactions: pd.DataFrame = bank_data.transaction_aggregations()
+    transactions_monthly: pd.DataFrame = bank_data.transaction_aggregations_monthly()
+
+    engine = create_engine("sqlite://", echo=False)
+
+    transactions.to_sql("transactions", con=engine)
+    transactions_monthly.to_sql("transactions_monthly", con=engine)
+
+    with engine.connect() as conn:
+        pprint(
+            conn.execute(
+                text(
+                    """SELECT account_number, trans_total FROM transactions 
+                WHERE trans_total = (SELECT MAX(trans_total) FROM transactions)"""
+                )
+            ).fetchall()
+        )
+
+        pprint(
+            conn.execute(
+                text(
+                    """SELECT account_number, n_transactions FROM transactions 
+                WHERE n_transactions = (SELECT MAX(n_transactions) FROM transactions)"""
+                )
+            ).fetchall()
+        )
+
+        pprint(
+            conn.execute(
+                text(
+                    """SELECT account_number, trans_total FROM transactions 
+                WHERE trans_total = (SELECT MIN(trans_total) FROM transactions)"""
+                )
+            ).fetchall()
+        )
+
+        pprint(
+            conn.execute(
+                text(
+                    "SELECT COUNT(account_number) FROM transactions WHERE n_transactions IS NULL"
+                )
+            ).fetchall()
+        )
 
 
 if __name__ == "__main__":
